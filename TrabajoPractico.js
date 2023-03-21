87968097192765418c75a6c0f5cd3d946dc36ece
Lucas Nahuel Lucero <lucaslucero2003@hotmail.com>
2023-03-21 18:29:19 -0300

    Segundo cambio

--- TrabajoPractico.js
+++ TrabajoPractico.js
@@ -4,10 +4,12 @@ let nombre = parametro.get('nombre');
 let apellido = parametro.get('apellido');
 let edad = parametro.get('edad');
 let dni = parametro.get('dni');
-let email = parametro.get('mail');
+let mail = parametro.get('mail');
+
+let hola = 0;
 
 function validarEdad(edad) {
-    if(edad < 18 || edad > 120){
+    if (edad < 18 || edad > 120) {
         return alert("Edad incorrecta");
     }
 } validarEdad(edad);
@@ -16,19 +18,53 @@ let largoNombre = nombre.length;
 let largoApellido = apellido.length;
 
 function validarNombres(largoNombre) {
-    if(largoNombre < 2 || largoNombre > 20){
+    if (largoNombre < 2 || largoNombre > 20) {
         return alert("Error en cantidad de letras nombre");
     }
-}validarNombres(largoNombre);
+} validarNombres(largoNombre);
 
 function validarApellido(largoApellido) {
-    if(largoApellido < 2 || largoApellido > 25){
+    if (largoApellido < 2 || largoApellido > 25) {
         return alert("Error en cantidad de letras apellido");
     }
-}validarNombres(largoApellido);
+} validarNombres(largoApellido);
 
 function validarDni(dni) {
-    if(dni < 100000){
+    if (dni < 100000) {
         return alert("DNI incorrecto");
     }
 } validarDni(dni);
+
+function validarEmail(mail) {
+    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail)) {
+        return true;
+    } else {
+        alert("El correo electrónico ingresado no es válido");
+        return false;
+    }
+} validarEmail(mail);
+
+document.getElementById("nom").innerHTML = nombre;
+document.getElementById("ape").innerHTML = apellido;
+document.getElementById("eda").innerHTML = edad;
+document.getElementById("dn").innerHTML = dni;
+document.getElementById("correo").innerHTML = mail;
+
+/*
+while (nombre!="fin") {
+    document.getElementById("nom").innerHTML = nombre;
+document.getElementById("ape").innerHTML = apellido;
+document.getElementById("eda").innerHTML = edad;
+document.getElementById("dn").innerHTML = dni;
+document.getElementById("correo").innerHTML = mail;
+}*/
+
+/*for (let i = 0; i < 5; i++) {
+    document.getElementById("nom").innerHTML = nombre;
+    document.getElementById("ape").innerHTML = apellido;
+    document.getElementById("eda").innerHTML = edad;
+    document.getElementById("dn").innerHTML = dni;
+    document.getElementById("correo").innerHTML = mail;
+    
+}*/
+
