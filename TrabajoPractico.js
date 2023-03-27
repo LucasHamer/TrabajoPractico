15b7d1214bc88fbc70acef61e8d8cf98eb8c8dbb
Lucas Nahuel Lucero <lucaslucero2003@hotmail.com>
2023-03-27 12:55:34 -0300

    Completo

--- TrabajoPractico.js
+++ TrabajoPractico.js
@@ -1,12 +1,10 @@
-let parametro = new URLSearchParams(window.location.search);
+/*let parametro = new URLSearchParams(window.location.search);
 
 let nombre = parametro.get('nombre');
 let apellido = parametro.get('apellido');
 let edad = parametro.get('edad');
 let dni = parametro.get('dni');
 let mail = parametro.get('mail');
-
-let hola = 0;
 
 function validarEdad(edad) {
     if (edad < 18 || edad > 120) {
@@ -44,27 +42,84 @@ function validarEmail(mail) {
     }
 } validarEmail(mail);
 
+/*
 document.getElementById("nom").innerHTML = nombre;
 document.getElementById("ape").innerHTML = apellido;
 document.getElementById("eda").innerHTML = edad;
 document.getElementById("dn").innerHTML = dni;
-document.getElementById("correo").innerHTML = mail;
+document.getElementById("correo").innerHTML = mail;*/
+let form = document.querySelector("form");
+let tableBody = document.querySelector("table tbody");
 
-/*
-while (nombre!="fin") {
-    document.getElementById("nom").innerHTML = nombre;
-document.getElementById("ape").innerHTML = apellido;
-document.getElementById("eda").innerHTML = edad;
-document.getElementById("dn").innerHTML = dni;
-document.getElementById("correo").innerHTML = mail;
-}*/
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-/*for (let i = 0; i < 5; i++) {
-    document.getElementById("nom").innerHTML = nombre;
-    document.getElementById("ape").innerHTML = apellido;
-    document.getElementById("eda").innerHTML = edad;
-    document.getElementById("dn").innerHTML = dni;
-    document.getElementById("correo").innerHTML = mail;
+    let nombre = form.nombre.value;
+    let apellido = form.apellido.value;
+    let edad = form.edad.value;
+    let dni = form.dni.value;
+    let email = form.mail.value;
+
+    function validarEdad(edad) {
+        if (edad < 18 || edad > 120) {
+            return alert("Edad incorrecta");
+        }
+    } validarEdad(edad);
     
-}*/
+    let largoNombre = nombre.length;
+    let largoApellido = apellido.length;
+    
+    function validarNombres(largoNombre) {
+        if (largoNombre < 2 || largoNombre > 20) {
+            return alert("Error en cantidad de letras nombre");
+        }
+    } validarNombres(largoNombre);
+    
+    function validarApellido(largoApellido) {
+        if (largoApellido < 2 || largoApellido > 25) {
+            return alert("Error en cantidad de letras apellido");
+        }
+    } validarNombres(largoApellido);
+    
+    function validarDni(dni) {
+        if (dni < 100000) {
+            return alert("DNI incorrecto");
+        }
+    } validarDni(dni);
+    
+    function validarEmail(email) {
+        if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+            return true;
+        } else {
+            alert("El correo electrónico ingresado no es válido");
+            return false;
+        }
+    } validarEmail(email);
+
+    let newRow = document.createElement("tr");
+
+    let nombreCell = document.createElement("td");
+    nombreCell.textContent = nombre;
+    newRow.appendChild(nombreCell);
+
+    let apellidoCell = document.createElement("td");
+    apellidoCell.textContent = apellido;
+    newRow.appendChild(apellidoCell);
+
+    let edadCell = document.createElement("td");
+    edadCell.textContent = edad;
+    newRow.appendChild(edadCell);
+
+    let dniCell = document.createElement("td");
+    dniCell.textContent = dni;
+    newRow.appendChild(dniCell);
+
+    let emailCell = document.createElement("td");
+    emailCell.textContent = email;
+    newRow.appendChild(emailCell);
+
+    tableBody.appendChild(newRow);
+
+    form.reset();
+});
 
